809b4b3586f232d7274e204e05e49121577abd37
Luiz Alberto <158484320+betuh4y@users.noreply.github.com>
2026-06-30 15:53:18 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Ajude minha Gatinha 🐱",
-  description: "Doe via PIX e ajude nos cuidados da minha gatinha",
+  title: "Ajude AVA 🐱",
+  description: "Doe via PIX e ajude nos cuidados de Ava",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
